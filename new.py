529a4f3fb51d0c993d70e0b00151c8d407074024
Ganesh-sacumen <ganesh.p@sacumen.com>
2024-03-19 12:45:37 +0530

    adding new files

---
+++ new.py
@@ -0,0 +1,18 @@
+""""Addition of Two Number."""
+
+
+def add(num1: int, num2: int) -> int:
+    """Addititon of two number
+
+    Args:
+        num1 (int): num1 is intreger type
+        num2 (int): num2 is intreger type
+      _
+
+    Returns:
+        int: returns addition for two number.
+    """
+    return num1 + num2
+
+
+add(10, 20)
